2ef40d7b6e2905e45f056cf302d45b6f42d8aed0
Wildson Seguchi <wseguchi@gmail.com>
2022-06-30 17:09:20 -0300

    Final files.

--- includes/main-js.js
+++ includes/main-js.js
@@ -1,112 +1,107 @@
+let tipPercentage, billValue, peopleValue, tipAmount, totalAmount;
 
-var tipPercentage;
-var billValue;
-var peopleValue;
-var tipAmount;
-var totalAmount;
-
-function input(id, varValue) {
-    checkInputValue(id, varValue);
-    getInputValue(id, varValue);
-    doTheMath();
+// Input field mask to allow only numbers:
+function inputMaskNumbers(evt) { 
+	evt = (evt) ? evt : window.event;
+	var charCode = (evt.which) ? evt.which : evt.keyCode;
+	if ((charCode > 7 && charCode < 58) || (charCode == 110) || (charCode == 190)) {
+		return true;
+	}
+	return false;
 }
 
-function getInputValue(id, varValue) {
-    console.log(varValue);
-    if (varValue == 'billValue') {
-        billValue = parseFloat(document.getElementById(id).value);
-    } else {
-        peopleValue = parseFloat(document.getElementById(id).value);
-    }
-} 
+// Check input value - Can't be zero:
+function checkInput(id, varValue) {
+	if (varValue == 0 || isNaN(varValue)) {
+		document.getElementById(id + '_zero').style.display = 'block';
+	} else {
+		document.getElementById(id + '_zero').style.display = 'none';
+	}
+}
+
+// This funticion is called by HTML input - "onInput":
+function input(id, varValue) {
+	checkInputValue(id, varValue);
+	getInputValue(id, varValue);
+	doTheMath();
+}
 
 function checkInputValue(id, varValue) {
-    varValue = parseFloat(document.getElementById(id).value);
-    console.log(varValue);
-    checkInput(id, varValue);
+	varValue = parseFloat(document.getElementById(id).value);
+	checkInput(id, varValue);
 }
 
+// Set values to JS variables: "billValue" and "peopleValue":
+function getInputValue(id, varValue) {
+	if (varValue == 'billValue') {
+		billValue = parseFloat(document.getElementById(id).value);
+	} else {
+		peopleValue = parseFloat(document.getElementById(id).value);
+	}
+}
+
+// Gets the tip percentage from buttons/custom input:
 function btnGetTipPercentage(id) {
-    tipPercentage = parseFloat(document.getElementById(id).value);
-    //console.log(tipPercentage);
-    doTheMath();
+	tipPercentage = parseFloat(document.getElementById(id).value);
+	doTheMath();
 }
 
-function inputMaskNumbers(evt) {
-    evt = (evt) ? evt : window.event;
-    var charCode = (evt.which) ? evt.which : evt.keyCode;
-    if ((charCode > 7 && charCode < 58) || (charCode == 110) || (charCode == 190)) {
-        return true;
-    }
-    return false;
-}
-
-function checkInput(id, varValue) {
-    if (varValue == 0 || isNaN(varValue)) {
-        document.getElementById(id+'_zero').style.display='block';
-    } else {
-        document.getElementById(id+'_zero').style.display='none';
-    }
-}
-
+// Time to do some math! =) 
 function doTheMath() {
-    console.log('1) billValue: ' + billValue);
-    console.log('2) tipPercentage: ' + tipPercentage);
-    console.log('3) peopleValue: ' + peopleValue);
+    // Calculate tip value:
+	tipAmount = billValue * (tipPercentage / 100);
+	if (isNaN(tipAmount)) {
+		document.getElementById('tip_amount_value').style.color = 'hsl(184, 80%, 24%)';
+		document.getElementById("tip_amount_value").innerHTML = '$0.00';
+	} else {
+		document.getElementById('tip_amount_value').style.color = 'hsl(172, 67%, 45%)';
+		document.getElementById("tip_amount_value").innerHTML = '$' + tipAmount.toFixed(2);
+	}
 
-    tipAmount = billValue * (tipPercentage / 100);
-    console.log('Resultado - tipAmount: ' + tipAmount);
-    if (isNaN(tipAmount)) {
-        document.getElementById('tip_amount_value').style.color='hsl(184, 80%, 24%)';
-        document.getElementById("tip_amount_value").innerHTML = '$0.00';
-    } else {
-        document.getElementById('tip_amount_value').style.color='hsl(172, 67%, 45%)';
-        document.getElementById("tip_amount_value").innerHTML = '$'+ tipAmount.toFixed(2);
-    }
-
-    totalAmount = (billValue * (1 + (tipPercentage / 100))) / peopleValue;
-    console.log('Resultado - totalAmount: ' + totalAmount);
-    if (isNaN(totalAmount)) {
-        document.getElementById('total_amount_value').style.color='hsl(184, 80%, 24%)';
-        document.getElementById("total_amount_value").innerHTML = '$0.00';
-    } else {
-        document.getElementById('total_amount_value').style.color='hsl(172, 67%, 45%)';
-        document.getElementById("total_amount_value").innerHTML = '$'+ totalAmount.toFixed(2);
-    }
+    // Calculate total amount per person:
+	totalAmount = (billValue * (1 + (tipPercentage / 100))) / peopleValue;
+	if (isNaN(totalAmount) || totalAmount == Infinity) {
+		document.getElementById('total_amount_value').style.color = 'hsl(184, 80%, 24%)';
+		document.getElementById("total_amount_value").innerHTML = '$0.00';
+	} else {
+		document.getElementById('total_amount_value').style.color = 'hsl(172, 67%, 45%)';
+		document.getElementById("total_amount_value").innerHTML = '$' + totalAmount.toFixed(2);
+	}
 }
 
+// Reset App:
 function btnReset() {
+	// Set all variable to zero:
+	tipPercentage = 0;
+	billValue = 0;
+	peopleValue = 0;
+	tipAmount = 0;
+	totalAmount = 0;
 
+	// Clear input values:
+	document.getElementById("bill_value").value = '';
+	document.getElementById("people_value").value = '';
+	document.getElementById("customp").value = '';
 
-    tipPercentage = 0;
-    billValue = 0;
-    peopleValue = 0;
-    tipAmount = 0;
-    totalAmount = 0;
+	// Clear output values and restore cdefault colors:
+	document.getElementById('tip_amount_value').style.color = 'hsl(184, 80%, 24%)';
+	document.getElementById("tip_amount_value").innerHTML = '$0.00';
+	document.getElementById('total_amount_value').style.color = 'hsl(184, 80%, 24%)';
+	document.getElementById("total_amount_value").innerHTML = '$0.00';
 
-    document.getElementById("bill_value").value = '';
-    document.getElementById("people_value").value = '';
-    document.getElementById("customp").value = '';
-
-    document.getElementById('tip_amount_value').style.color='hsl(184, 80%, 24%)';
-    document.getElementById("tip_amount_value").innerHTML = '$0.00';
-
-    document.getElementById('total_amount_value').style.color='hsl(184, 80%, 24%)';
-    document.getElementById("total_amount_value").innerHTML = '$0.00';
-
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-
-    document.getElementById("customp").classList.add("active");
-
+	// Tip Selector Buttons - Clear "active" class from all buttons and add it to "customp" div:
+	var current = document.getElementsByClassName("active");
+	current[0].className = current[0].className.replace(" active", "");
+	document.getElementById("customp").classList.add("active");
 }
 
+// Highlight active button:
 var header = document.getElementById("buttons_grid");
 var btns = header.getElementsByClassName("btn-act");
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
-  });
+	btns[i].addEventListener("click", function() {
+		var current = document.getElementsByClassName("active");
+		current[0].className = current[0].className.replace(" active", "");
+		this.className += " active";
+	});
 }
